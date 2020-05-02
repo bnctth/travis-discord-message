de05200fff62efc5f500ba874a5d49b6a0958697
Bence <btoth0211@gmail.com>
2020-05-02 14:36:36 +0200

    author

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"os/exec"
 	"time"
 )
 
@@ -42,6 +43,15 @@ func main() {
 	message := flag.String("message", "¯\\_(ツ)_/¯", "The text of the message")
 	flag.Parse()
 
+	// Getting frequently used env variables
+	branch := os.Getenv("TRAVIS_BRANCH")
+	repo := os.Getenv("TRAVIS_REPO_SLUG")
+	commit := os.Getenv("TRAVIS_COMMIT")
+
+	comm := exec.Command("git", "log", "-1", "--pretty=\"%aN\"")
+	output, err := comm.Output()
+	author := string(output)[1 : len(output)-2]
+
 	// The request payload based on https://discordapp.com/developers/docs/resources/webhook
 	payload := object{
 		"avatar_url": fmt.Sprintf("https://travis-ci.org/images/logos/TravisCI-Mascot-%d.png", rand.Intn(3)+1),
@@ -50,14 +60,14 @@ func main() {
 				"title": *message,
 				"color": ternary(*success, 0x81f207, 0xf2073a),
 				"author": object{
-					"name":     fmt.Sprintf("Job %s on branch %s at %s", os.Getenv("TRAVIS_JOB_NUMBER"), os.Getenv("TRAVIS_BRANCH"), os.Getenv("TRAVIS_REPO_SLUG")),
+					"name":     fmt.Sprintf("Job %s on branch %s at %s", os.Getenv("TRAVIS_JOB_NUMBER"), branch, repo),
 					"url":      os.Getenv("TRAVIS_BUILD_WEB_URL"),
 					"icon_url": fmt.Sprintf("https://travis-ci.org/images/logos/TravisCI-Mascot-%s.png", ternary(*success, "blue", "red")),
 				},
 				"fields": []object{
 					{
 						"name":   "Commit",
-						"value":  os.Getenv("TRAVIS_COMMIT"),
+						"value":  fmt.Sprintf("[%s](https://github.com/%s/commit/%s)", commit[:7], repo, commit),
 						"inline": true,
 					},
 					{
@@ -67,12 +77,17 @@ func main() {
 					},
 					{
 						"name":   "Branch",
-						"value":  os.Getenv("TRAVIS_BRANCH"),
+						"value":  fmt.Sprintf("[%s](https://github.com/%s/tree/%s)", branch, repo, branch),
 						"inline": true,
 					},
 					{
 						"name":   "Stage",
 						"value":  os.Getenv("TRAVIS_BUILD_STAGE_NAME"),
+						"inline": true,
+					},
+					{
+						"name":   "Author",
+						"value":  author,
 						"inline": true,
 					},
 				},
